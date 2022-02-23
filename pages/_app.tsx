fc04b071fbd818a25628bfedf557ad29dec6c81e
hanbin9775 <dnffkrktyd@gmail.com>
2022-02-23 23:49:20 +0900

    add: recoil, next, typescript setting

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import { RecoilRoot } from "recoil";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
+}
+
+export default MyApp;
